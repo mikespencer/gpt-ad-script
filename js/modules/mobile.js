f0568120167f31ea186b6b44cae755e7725b4c14
Adam Levy <adam.levy@washpost.com>
2013-03-13 12:11:19 -0400

    added sniff module for mobile device checking in the future. incorporated to mobile.js as wpAd.sniff

--- js/modules/mobile.js
+++ js/modules/mobile.js
@@ -6,16 +6,18 @@
   'use strict';
 
   if(typeof define === 'function'){
-    define('mobile', ['generic.core', 'json!mobile.config.json', 'utils.core'], function(wpAd, config, utils){
-      
+    define('mobile', ['generic.core', 'json!mobile.config.json', 'utils.core', 'sniff'], function(wpAd, config, utils, sniff){
+
       //set the base node
       wpAd.dfpSite = '/701/mob.wp.';
-      
+
       //expose helper functions
       wpAd.utils = utils;
-      
+
       //expose config
       wpAd.config = config;
+
+      wpAd.sniff = sniff;
 
       return wpAd;
     });
---
+++ js/modules/sniff.js
@@ -0,0 +1,52 @@
+(function(w, d, n, define) {
+
+    'use strict';
+
+    if (typeof define === "function") {
+
+        /**
+         * Set of functions that return various pieces ofdevice info
+         * @class sniff
+         */
+        define(function() {
+
+            var iuaindex, auaindex, ual, ua = n.userAgent;
+
+            var sniff = {
+
+                isApple: function() {
+                     return ua.match(/(iPad|iPhone|iPod)/g) ? true : false;
+                },
+                isAndroid: function() {
+                    ual = ua.toLowerCase();
+                    return ual.match(/(android)/g) ? true : false;
+                },
+                iDeviceType: function() {
+                    if (ua.match(/(iPad)/g)) {
+                        return "iPad";
+                    } else if (ua.match(/(iPhone)/g)){
+                        return "iPhone";
+                    } else if (ua.match(/(iPod)/g)){
+                        return "iPod";
+                    } else {
+                        return false;
+                    }
+                },
+                hasOSVersion: function() {
+                    auaindex = ua.indexOf( 'Android ' );
+                    iuaindex = ua.indexOf( 'OS ' );
+                    if (this.sniff.isApple()  &&  iuaindex > -1 ) {
+                        return ua.substr( iuaindex + 3, 3 ).replace( '_', '.' );
+                    } else if (this.sniff.isAndroid() && auaindex > -1 ){
+                        return ua.substr( auaindex + 8, 3 );
+                    } else {
+                        return false;
+                    }
+                }
+            };
+            return sniff;
+        });
+    }
+
+
+})(window, document, navigator, window.define);
