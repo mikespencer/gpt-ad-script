f150a84f0375634012600bed8572502929a72e08
mikespencer <michael.spencer@washingtonpost.com>
2013-05-16 17:58:59 -0400

    updates

---
+++ js/utils/setTargeting.js
@@ -0,0 +1,16 @@
+define(['utils/isArray'], function(isArray){
+
+  /**
+   * Assigns keyvalues to a the gpt publisher service, or a gpt ad slot
+   * @param {Object} map Key/Value mapping
+   * @param {Object} target GPT publisher service, or GPT ad slot
+   */
+  return function(map, target){
+    for(var key in map){
+      if(map.hasOwnProperty(key)){
+        target.setTargeting(key, isArray(map[key]) ? map[key] : [map[key]]);
+      }
+    }
+  };
+
+});
