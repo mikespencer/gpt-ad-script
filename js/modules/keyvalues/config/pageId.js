7fe7187a8f68c4f5dc738f428050296a593b0998
mikespencer <michael.spencer@washingtonpost.com>
2013-06-19 16:04:18 -0400

    page_id keyvalue function updat

--- js/modules/keyvalues/config/pageId.js
+++ js/modules/keyvalues/config/pageId.js
@@ -1,11 +1,8 @@
 define(['utils'], function(utils){
 
   return function(){
-    var l = utils.wp_meta_data.page_id ? utils.wp_meta_data.page_id.length : [];
-    while(l--){
-      utils.wp_meta_data.page_id[l] = utils.wp_meta_data.page_id[l].toString().replace(/\./g, '_');
-    }
-    return utils.wp_meta_data.page_id;
+    var page_id = utils.wp_meta_data.page_id;
+    return page_id ? [ page_id.toString().replace(/\./g, '_') ] : [];
   };
 
 });
