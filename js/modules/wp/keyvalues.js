1f9ea5350446539258476d64afd5b8d9a5ca403a
mikespencer <michael.spencer@washingtonpost.com>
2013-06-25 17:32:52 -0400

    wp keyvalues added

--- js/modules/wp/keyvalues.js
+++ js/modules/wp/keyvalues.js
@@ -1,7 +1,7 @@
 /**
  * Extends universal desktop, page level keyvalues with wp desktop specific keyvalues
  */
-define(['keyvalues/desktop', 'utils'], function(kvs, utils){
+define(['keyvalues/desktop', 'utils', 'zoneBuilder'], function(kvs, utils, zoneBuilder){
 
   return utils.merge(kvs, {
 
@@ -9,6 +9,23 @@ define(['keyvalues/desktop', 'utils'], function(kvs, utils){
       function(){
         return (/washingtonpost\.com|personalpost|obituaries|weather|jobs\/search/).test(commercialNode) ?
           ['intrusive'] : false;
+      }
+    ],
+
+    author: [
+      function(){
+        var author = utils.wp_meta_data.author,
+          rv = [],
+          l, i;
+        if(author){
+          l = author.length;
+          for(i=0;i<l;i++){
+            if(author[i]){
+              rv.push(author[i].replace(/[^\w\s]/gi, '').replace(/\s/g,"_").toLowerCase());
+            }
+          }
+        }
+        return rv;
       }
     ],
 
@@ -20,6 +37,18 @@ define(['keyvalues/desktop', 'utils'], function(kvs, utils){
           id = [a[a.length - 1].toLowerCase().split("_story")[0]];
         }
         return id;
+      }
+    ],
+
+    de: [
+      function(){
+        var cookie = utils.getCookie('de');
+        if(cookie){
+          cookie = unescape(cookie);
+          cookie = cookie.split(':');
+          return cookie.length ? cookie : false;
+        }
+        return false;
       }
     ],
 
@@ -39,6 +68,12 @@ define(['keyvalues/desktop', 'utils'], function(kvs, utils){
           }
         }
         return rv;
+      }
+    ],
+
+    page: [
+      function(){
+        return utils.wp_meta_data.contentType && zoneBuilder.contentType[utils.wp_meta_data.contentType.toString().toLowerCase()] || ['article'];
       }
     ],
 
@@ -62,6 +97,74 @@ define(['keyvalues/desktop', 'utils'], function(kvs, utils){
         }
 
         return rv;
+      }
+    ],
+
+    areaId: [
+      function(){
+        var a = utils.urlCheck('areaId', {
+          type: 'variable'
+        }), rv = [];
+        if(window.hs && window.hs.geo_area_id) {
+          rv = win.hs.geo_area_id.split(';');
+        }
+        if(a) {
+          rv.push(a);
+        }
+        return rv;
+      }
+    ],
+
+    aptco: [
+      function(){
+        var a = utils.urlCheck('aptco', {
+          type: 'variable'
+        });
+        return a ? [a] : [];
+      }
+    ],
+
+    metro: [
+      function(){
+        var a = utils.urlCheck('metro', {
+          type: 'variable'
+        });
+        return a ? [a] : [];
+      }
+    ],
+
+
+    //wtf is this...? need to check on this to see if we can remove it
+    locExpSponsor: [
+      function(){
+        var rv = [];
+        if(window.countyName && window.stateName) {
+          var invalidKW = ['?', '=', '/', '\\', ':', ';', ',', '*', '(', ')', '&', '$', '%', '@',
+            '!', '^', '+', ' ', '[', ']', '{', '}', '.'],
+            l = invalidKW.length,
+            csRE;
+
+          while(l--) {
+            csRE = new RegExp('(\\' + invalidKW[l] + ')', 'g');
+            countyName = countyName.replace(csRE, "").toLowerCase();
+            stateName = stateName.replace(csRE, "").toLowerCase();
+          }
+
+          rv = [window.countyName + "-" + window.stateName];
+        }
+        return rv;
+      }
+    ],
+
+    ppwidget: [
+      function(){
+        return utils.urlCheck('tid', {type:'variable'}) === 'pp_stream' ? '1' : '';
+      }
+    ],
+
+    wpnode: [
+      function(){
+        return window.commercialNode;
       }
     ]
 
