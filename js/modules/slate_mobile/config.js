41cf554fa1c1ecc1b5842c82ed854bc94c01bb87
mikespencer <michael.spencer@washingtonpost.com>
2013-10-07 11:12:04 -0400

    updating mobile ad spots

--- js/modules/slate_mobile/config.js
+++ js/modules/slate_mobile/config.js
@@ -1,31 +1,34 @@
 /**
- *  Template of ad flights and available ad spots on washingtonpost.com (mobile web)
+ *  Template of ad flights and available ad spots on slate.com (mobile web)
  */
 define(function(){
   return {
     flights: {
       defaults: {
-        what: ['b', 'fixedBottom', 't']
+        what: ['b', 'fixedBottom', 't', 'm']
       }
     },
     adTypes: {
       'b': {
         'size': [
-          [300,50],
-          [320, 50],
-          [1,1]
+          [320, 50]
         ]
       },
       'fixedBottom': {
         'size': [
-          [300,50],
+          [300, 50],
           [320, 50],
-          [1,1]
+          [1, 1]
         ]
       },
       't': {
         'size': [
           [320, 50]
+        ]
+      },
+      'm': {
+        'size': [
+          [300, 250]
         ]
       }
     }
