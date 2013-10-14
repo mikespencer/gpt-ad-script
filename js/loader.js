28d5b93a60e8a912d9ab454280f3e19ee6defd8d
mikespencer <michael.spencer@washingtonpost.com>
2013-10-14 16:01:05 -0400

    css update and enabling stylesheet for wp mobile

--- js/loader.js
+++ js/loader.js
@@ -54,7 +54,7 @@ var placeAd2, wpAd = wpAd || {}, googletag = googletag || { cmd: [] };
       wp: true,
       theroot: false,
       slate: false,
-      wp_mobile: false,
+      wp_mobile: true,
       slate_mobile: false
     },
 
