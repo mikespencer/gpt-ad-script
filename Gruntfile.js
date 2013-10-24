a1ffd5720a6f51e9c091a3bf17a8ebf76409fcae
Mike Spencer <mikespencema@gmail.com>
2013-10-24 15:31:27 -0400

    minor tweak

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,3 @@
-/* global connect: true, static: true */
-
 var LIVERELOAD_PORT = 35729;
 var lrSnippet = require('connect-livereload')({port: LIVERELOAD_PORT});
 var mountFolder = function (connect, dir) {
