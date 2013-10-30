104d8f0d5b19fbbbeeb8c08b97a8d59b7bfe0dca
Mike Spencer <mikespencema@gmail.com>
2013-10-30 12:49:20 -0400

    gruntfile jshint fix

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,4 +1,4 @@
-/*jshint ec5:true */
+/*jshint node:true */
 var LIVERELOAD_PORT = 35729;
 var lrSnippet = require('connect-livereload')({port: LIVERELOAD_PORT});
 var mountFolder = function (connect, dir) {
