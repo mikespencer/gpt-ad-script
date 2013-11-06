1dbea7b1f0e63653210c0d47325067342351f948
Mike Spencer <mikespencema@gmail.com>
2013-11-06 13:46:05 -0500

    porting over more hackbin parts

--- js/modules/wp/overrides.js
+++ js/modules/wp/overrides.js
@@ -1,23 +1,28 @@
 /**
  * Overrides for standard configuration of ad spots for unique circumstances on washingtonpost.com (desktop)
  */
-define(['utils'], function(utils) {
+define(['utils', 'wp/flags', 'jQuery'], function(utils, flags, $) {
 
   /**
    * Object of checks for overrides
    */
   return {
+
     what: {
+
       tiffany_tile: function(){
-        if (this.config.where === "washingtonpost.com") {
+        if (flags.pageType.homepage) {
           this.config.size = [[184,90]];
         }
         //important to disable carousel
         window.wpTiles = window.wpTiles || {};
         window.wpTiles.hasTiff = true;
       }
+
     },
+
     pos: {
+
       //determines if page gets a full page interstitial
       interstitial: function(){
         if(utils.flags.showInterstitial && !utils.flags.front){
@@ -26,8 +31,9 @@ define(['utils'], function(utils) {
           });
         }
       },
+
       leaderboard: function() {
-        if (this.config.where === 'washingtonpost.com') {
+        if (flags.pageType.homepage) {
           this.config.where += '/lb';
           if(utils.flags.reload){
             this.config.where += 'refresh';
@@ -37,15 +43,16 @@ define(['utils'], function(utils) {
           del: ['js']
         });
       },
+
       featrent: function() {
-        var adi_div = document.getElementById('wpni_adi_featrent');
-        if(adi_div){
-          adi_div.style.background = 'none';
-          adi_div.style.padding = '0';
-        }
+        $('#wpni_adi_featrent').css({
+          background: 'none',
+          padding: '0'
+        });
       },
+
       flex_bb_hp: function() {
-        if(this.config.where === 'washingtonpost.com'){
+        if(flags.pageType.homepage){
           this.config.where += '/hpflex';
           if(utils.flags.reload){
             this.config.where += 'refresh';
@@ -59,24 +66,50 @@ define(['utils'], function(utils) {
           del: ['js']
         });
       },
+
       flex_ss_bb_hp: function(){
         this.addKeyvalues({
           del: ['js']
         });
       },
+
       pushdown: function(){
-        if(this.config.where === 'washingtonpost.com'){
-          var adi_push = document.getElementById('wpni_adi_pushdown');
-          if (adi_push) {
-            adi_push.style.backgroundImage = 'url(http://img.wpdigital.net/wp-adv/test/mstest/pushdown-ad-small.png)';
-            adi_push.style.backgroundPosition = '-7px -100px';
-          }
+        if(flags.pageType.homepage){
+          $('#wpni_adi_pushdown').css({
+            backgroundImage: 'url(http://img.wpdigital.net/wp-adv/test/mstest/pushdown-ad-small.png)',
+            backgroundPosition: '-7px -100px'
+          });
+          //eyeblaster specific fix for their expanded pushdown overlaying our expanded nav
+          $(window).load(function(){
+            if($('#slug_pushdown div[id^="ebDiv"]').length){
+              $('#main-nav>li').not('.jobs, .posttv').hover(function(){
+                $('#eyeDiv div[id^="eb"]').find('object, embed').each(function(){
+                  $(this).parent().addClass('adnoDisplay');
+                });
+              }, function(){
+                $('#eyeDiv div[id^="eb"]').find('object, embed').each(function(){
+                  $(this).parent().removeClass('adnoDisplay');
+                });
+              });
+            }
+          });
         }
+      },
+
+      persistent_bb: function(){
+        var appendTimer = setTimeout(function(){
+          $('#wpni_adi_persistent_bb').append('<a href="//games.washingtonpost.com?wpmk=MK0000246" target="_blank" style="display:block;margin-top:5px;">' +
+            '<img src="//img.wpdigital.net/wp-srv/ad/img/games_300x250.jpg" width="300" height="250" alt="WP Games - Click Here for More!" />' +
+          '</a>');
+        }, 500);
       }
+
     },
+
     where: {
-      // May not need this section..
+      // May not need this section...
     }
+
   };
 
 });
