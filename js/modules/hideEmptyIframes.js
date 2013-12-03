6c97927d00494fbf2791f207290bab22ad8d3f2b
Mike Spencer <mikespencema@gmail.com>
2013-12-03 15:58:36 -0500

    tweaked check for iframe contents

--- js/modules/hideEmptyIframes.js
+++ js/modules/hideEmptyIframes.js
@@ -4,7 +4,7 @@ define(['jQuery'], function($){
       selector = selector || 'iframe[id^="google_ads_iframe"]:visible';
       $(selector).each(function(i, el){
         var $this = $(this),
-          hasContent = $this.contents().find('body *').length;
+          hasContent = $this.contents().find('body').contents().length;
         if(!hasContent){
           $this.hide().closest('[id^="slug_"]').hide();
         }
